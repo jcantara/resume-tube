50d82ae68d6f2e5cc9ffbdb69c6cbb1f2f928325
Jesse <jcantara@gmail.com>
2016-02-14 22:38:23 -0500

    WIP

--- server/resume-tube.js
+++ server/resume-tube.js
@@ -1,2 +1,17 @@
 Meteor.startup(function () {
+
+ServiceConfiguration.configurations.upsert(
+  { service: "google" },
+  {
+    $set: {
+      clientId: Meteor.settings.googleClientId,
+      loginStyle: "popup",
+      secret: Meteor.settings.googleClientSecret
+    }
+  }
+);
+
+// ensure indexes:
+Playlists._ensureIndex({ "userId": 1});
+
 });
